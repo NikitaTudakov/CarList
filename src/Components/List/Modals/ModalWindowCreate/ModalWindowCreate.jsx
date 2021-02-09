a36b97d50209bf15787558260c4d6169f4c54762
Nikita <tudakov.nikita@gmail.com>
2021-02-09 15:49:15 +0200

    Bug fixing

--- src/Components/List/Modals/ModalWindowCreate/ModalWindowCreate.jsx
+++ src/Components/List/Modals/ModalWindowCreate/ModalWindowCreate.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import {useStyles} from '../ModalStyle';
+import makeStyles from '@material-ui/core/styles/makeStyles';
 
 import Modal from '@material-ui/core/Modal';
 import TextField from '@material-ui/core/TextField';
@@ -31,6 +31,45 @@ export const ModalWindowCreate = ({ handleClose, open, }) => {
       setDisabled(true);
     }
   };
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    position: 'absolute',
+    width: 775,
+    backgroundColor: theme.palette.background.paper,
+    'border-radius': 4,
+    padding: theme.spacing(3, 4, 3),
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  textField: {
+    width: '338px',
+    margin: theme.spacing(2),
+  },
+
+  footer: {
+    display: 'flex',
+    justifyContent: 'flex-end',
+    margin: theme.spacing(2),
+  },
+
+  header: {
+    display: 'flex',
+    justifyContent: 'space-between',
+  },
+
+  'button-main': {
+    marginLeft: '10px',
+    backgroundColor: '#22B2BE',
+    "&:hover": {
+      backgroundColor: '#22B2BE',
+      opacity: '0.8'
+    }
+  },
+}));
 
   const classes = useStyles();
 
--- src/Components/List/Modals/ModalWindowEdit/ModalWindowEdit.jsx
+++ src/Components/List/Modals/ModalWindowEdit/ModalWindowEdit.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { editCar } from '../../../../api';
-import {useStyles} from '../ModalStyle';
+import makeStyles from '@material-ui/core/styles/makeStyles';
 
 import Modal from '@material-ui/core/Modal';
 import TextField from '@material-ui/core/TextField';
@@ -35,9 +35,46 @@ export const ModalWindowEdit = ({ handleClose, open, car }) => {
     }
   };
 
+  const useStyles = makeStyles((theme) => ({
+    paper: {
+      position: 'absolute',
+      width: 775,
+      backgroundColor: theme.palette.background.paper,
+      'border-radius': 4,
+      padding: theme.spacing(3, 4, 3),
+      top: '50%',
+      left: '50%',
+      transform: 'translate(-50%, -50%)',
+      display: 'flex',
+      flexDirection: 'column',
+    },
+    textField: {
+      width: '338px',
+      margin: theme.spacing(2),
+    },
+  
+    footer: {
+      display: 'flex',
+      justifyContent: 'flex-end',
+      margin: theme.spacing(2),
+    },
+  
+    header: {
+      display: 'flex',
+      justifyContent: 'space-between',
+    },
+  
+    'button-main': {
+      marginLeft: '10px',
+      backgroundColor: '#22B2BE',
+      "&:hover": {
+        backgroundColor: '#22B2BE',
+        opacity: '0.8'
+      }
+    },
+  }));
+
   const classes = useStyles();
-
-
 
   return (
     <Modal
